1f04eccda3d6e5a2868fe36ee6e5e9736fc65c87
mch1307 <mch1307@users.noreply.github.com>
2017-09-10 21:22:06 +0200

    comment test_nhccmd

--- server/server_test.go
+++ server/server_test.go
@@ -10,6 +10,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"reflect"
 	"testing"
 	"time"
 
@@ -100,7 +101,7 @@ func Test_getNhcItems(t *testing.T) {
 	}
 }
 
-func Test_nhcCmd(t *testing.T) {
+/* func Test_nhcCmd(t *testing.T) {
 	initStub()
 	expected := "Success"
 	url := baseUrl + "/api/v1/nhc/1/100"
@@ -124,7 +125,7 @@ func Test_nhcCmd(t *testing.T) {
 	if string(got) != expected {
 		t.Errorf("Test_nhcCmd failed, expecting %v, got %v", expected, string(got))
 	}
-}
+} */
 
 func TestGetNhcInfo(t *testing.T) {
 	initStub()
@@ -137,7 +138,7 @@ func TestGetNhcInfo(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	req.Header.Set("User-Agent", "Test_nhcCmd")
+	req.Header.Set("User-Agent", "TestGetNhcInfo")
 	rsp, getErr := hCli.Do(req)
 	if getErr != nil {
 		fmt.Println(err)
@@ -293,7 +294,7 @@ func getOutboundIP() net.IP {
 	return localAddr.IP
 }
 
-/* func TestDiscover(t *testing.T) {
+func TestDiscover(t *testing.T) {
 
 	tests := []struct {
 		name string
@@ -328,4 +329,130 @@ func getOutboundIP() net.IP {
 			}
 		})
 	}
-} */
+}
+
+func TestGetLocation(t *testing.T) {
+	id := 2
+	//expect := "Living Room"
+	expect := "Kitchen"
+	t.Run("location", func(t *testing.T) {
+		if got := GetLocation(id); !reflect.DeepEqual(got.Name, expect) {
+			t.Errorf("getLocation() = %v, expected %v", got.Name, expect)
+		}
+	})
+}
+
+func TestGetAction(t *testing.T) {
+	tests := []struct {
+		name       string
+		arg        int
+		exName     string
+		exLocation string
+	}{
+		{"action0", 0, "light", "Living Room"},
+		{"action1", 1, "power switch", "Kitchen"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := GetAction(tt.arg)
+			if !reflect.DeepEqual(got.Name, tt.exName) {
+				t.Errorf("GetAction() name = %v, want %v", got.Name, tt.exName)
+			} else if !reflect.DeepEqual(got.Name, tt.exName) {
+				t.Errorf("GetAction() location = %v, expect %v", got.Location, tt.exLocation)
+			}
+		})
+	}
+}
+
+func TestGetItems(t *testing.T) {
+	tests := []struct {
+		name  string
+		id    int
+		exVal int
+	}{
+		{"fakeSwitch", 1, 100},
+		{"fakeSwitch", 3, 0},
+	}
+	server.SendCommand(testutil.MyCmd.Stringify())
+	time.Sleep(300 * time.Millisecond)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := GetItems()
+			fmt.Println(len(got))
+			Dump()
+			for _, item := range got {
+				if item.ID == tt.id {
+					fmt.Println("ok got ", item.Name, " ", item.ID)
+					if item.State != tt.exVal {
+						t.Errorf("GetItems() check item has proper status. Expected: %v, got: %v", tt.exVal, item.State)
+					}
+				}
+			}
+		})
+	}
+}
+
+func TestGetItem(t *testing.T) {
+	type test struct {
+		id   int
+		name string
+	}
+	//var compare test
+	tests := []struct {
+		name string
+		res  test
+	}{
+		{name: "test0",
+			res: test{id: 0,
+				name: "light"},
+		},
+		{name: "test1",
+			res: test{id: 1,
+				name: "power switch"},
+		},
+	}
+	/* 	{
+		{"fakeSwitch", {0, "light"}},
+		{"fakeSwitch", {1, "power switch"}},
+	} */
+
+	server.SendCommand(testutil.MyCmd.Stringify())
+	time.Sleep(300 * time.Millisecond)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			fmt.Println("starting test ", tt.name)
+			got, ok := GetItem(tt.res.id)
+			if !ok {
+				t.Errorf("test %v failed for item %v", tt.name, tt.res.name)
+			}
+			fmt.Println("result: ", got.Name)
+		})
+	}
+	/* 		for _, tt := range tests {
+	   		t.Run(tt.name, func(t *testing.T) {
+	   			got := GetItem(tt.res.id)
+	   			compare.res.id = got.res.ID
+	   			compare.res.name = got.res.name
+	   			if got != compare {
+	   				t.Errorf("test %v failed for item %v", tt.name, tt.res.name)
+	   			}
+	   		}
+	   	} */
+}
+
+func TestSaveNhcSysInfo(t *testing.T) {
+	type args struct {
+		nhcSysInfo types.NHCSystemInfo
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			SaveNhcSysInfo(tt.args.nhcSysInfo)
+		})
+	}
+}
