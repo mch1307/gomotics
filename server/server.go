479bb6f6fce2b7f590a21b5e157d7bc0c58200ff
mch1307 <mch1307@users.noreply.github.com>
2017-10-31 21:58:16 +0100

    add comments for swagger doc

--- server/server.go
+++ server/server.go
@@ -25,17 +25,6 @@ type Server struct {
 	LogFile    string
 }
 
-// Start Initialize and starts the server
-func Start(conf string) {
-	config.Initialize(conf)
-	log.Init()
-	s := Server{}
-	s.Initialize()
-	s.Run()
-	NhcInit(&config.Conf.NhcConfig)
-	fmt.Println("Starting gomotics")
-}
-
 // Initialize initialize the server
 // also calls the internal in mem db
 func (s *Server) Initialize() {
@@ -50,28 +39,114 @@ func (s *Server) Initialize() {
 
 func (s *Server) intializeRoutes() {
 	s.Router.HandleFunc("/health", Health).Methods("GET")
+	// swagger:route GET /nhc List NHC registered items
+	//
+	// Lists all registered NHC items.
+	//
+	// This will show all registered NHC items.
+	//
+	//     Consumes:
+	//     - application/json
+	//     - application/x-protobuf
+	//
+	//     Produces:
+	//     - application/json
+	//     - application/x-protobuf
+	//
+	//     Schemes: http, https, ws, wss
+	//
+	//
+	//     Responses:
+	//       default: genericError
+	//       200: someResponse
+	//       422: validationError
 	s.Router.HandleFunc("/api/v1/nhc/", GetNhcItems).Methods("GET")
+	// swagger:route GET /nhc/{id:[0-9]+} Find NHC registered item with specific ID
+	//
+	// Find NHC registered item with specific ID
+	//
+	// This will show all details of a given registered NHC item.
+	//
+	//     Consumes:
+	//     - application/json
+	//     - application/x-protobuf
+	//
+	//     Produces:
+	//     - application/json
+	//     - application/x-protobuf
+	//
+	//     Schemes: http, https, ws, wss
+	//
+	//
+	//     Responses:
+	//       default: genericError
+	//       200: someResponse
+	//       422: validationError
 	s.Router.HandleFunc("/api/v1/nhc/{id:[0-9]+}", GetNhcItem).Methods("GET")
+	// swagger:route POST /nhc/{id:[0-9]+}/{value:[0-9]+} Update given NHC id with provided value. on-off -> 0-100, intermediate value for dimmer device.
+	//
+	//
+	//     Produces:
+	//     - application/json
+	//     - application/x-protobuf
+	//
+	//     Schemes: http, https, ws, wss
+	//
+	//
+	//     Responses:
+	//       default: genericError
+	//       200: someResponse
+	//       422: validationError
 	s.Router.HandleFunc("/api/v1/nhc/{id:[0-9]+}/{value:[0-9]+}", NhcCmd).Methods("POST")
-	//s.Router.HandleFunc("/api/v1/nhc/action", nhc.NhcCmd).Methods("PUT")
+	// swagger:route POST /nhc/info Get NHC controller info
+	//
+	//
+	//     Consumes:
+	//     - application/json
+	//     - application/x-protobuf
+	//
+	//     Produces:
+	//     - application/json
+	//     - application/x-protobuf
+	//
+	//     Schemes: http, https, ws, wss
+	//
+	//
+	//     Responses:
+	//       default: genericError
+	//       200: someResponse
+	//       422: validationError
 	s.Router.HandleFunc("/api/v1/nhc/info", GetNhcInfo).Methods("GET")
+	// swagger:route GET /events Websocket endpoint providing updated items in realtime
+	//
+	// Update on items in realtime
+	//
+	// Websocket endpoint providing updated items in realtime.
+	//
+	//
+	//
+	//
+	//
+	//     Produces:
+	//     - application/json
+	//
+	//
+	//     Schemes: ws, wss
+	//
+	//
+	//     Responses:
+	//
+	//
+	//
 	s.Router.HandleFunc("/events", ServeWebSocket).Methods("GET")
 
 	s.Router.Walk(func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
 		t, _ := route.GetPathTemplate()
-		/* 		if err != nil {
-			return err
-		} */
 		// p will contain regular expression is compatible with regular expression in Perl, Python, and other languages.
 		// for instance the regular expression for path '/articles/{id}' will be '^/articles/(?P<v0>[^/]+)$'
 		p, _ := route.GetPathRegexp()
-		/* 		if err != nil {
-			return err
-		} */
 		m, _ := route.GetMethods()
-		/* 		if err != nil {
-			return err
-		} */
+
 		fmt.Println(strings.Join(m, ","), t, p)
 		log.Info(strings.Join(m, ","), t, p)
 		return nil
@@ -82,6 +157,17 @@ func (s *Server) intializeRoutes() {
 func (s *Server) Run() {
 	go NhcListener()
 	log.Fatal(http.ListenAndServe(s.ListenPort, s.Router))
+}
+
+// Start Initialize and start the server
+func Start(conf string) {
+	config.Initialize(conf)
+	log.Init()
+	s := Server{}
+	s.Initialize()
+	s.Run()
+	NhcInit(&config.Conf.NhcConfig)
+	fmt.Println("Starting gomotics")
 }
 
 // Health endpoint for health monitoring
